86e75a1db3bfbd47aad85490823896aa7522d87d
Higa <codehiga@gmail.com>
2022-10-09 23:01:53 -0300

    sut criado a partir de uma factory

--- src/domain/usecases/local-save-purchases.spec.ts
+++ src/domain/usecases/local-save-purchases.spec.ts
@@ -17,11 +17,30 @@ class CacheStoreSpy implements CacheStore {
   }
 }
 
+type SutTypes = {
+  sut: LocalSavePurchases;
+  cacheStore: CacheStoreSpy;
+};
+
+//Design pattern = factory
+const makeSut = (): SutTypes => {
+  const cacheStore = new CacheStoreSpy();
+  const sut = new LocalSavePurchases(cacheStore);
+  return {
+    sut,
+    cacheStore,
+  };
+};
+
 describe("LocalSavePurchases", () => {
   test("Should not delete cache on sut.init", () => {
-    const cacheStoreSpy = new CacheStoreSpy();
-    new LocalSavePurchases(cacheStoreSpy);
+    const { cacheStore } = makeSut();
+    expect(cacheStore.deleteCallsCount).toBe(0);
+  });
 
-    expect(cacheStoreSpy.deleteCallsCount).toBe(0);
+  test("Should delete old cache on sut.save", async () => {
+    const { cacheStore, sut } = makeSut();
+    await sut.save();
+    expect(cacheStore.deleteCallsCount).toBe(1);
   });
 });
